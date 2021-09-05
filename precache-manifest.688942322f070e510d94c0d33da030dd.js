1938b563257e88cfb7936efd33d13b4887a3274a
Code Sandbox <hello@codesandbox.io>
2021-09-05 05:14:25 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.688942322f070e510d94c0d33da030dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ff24a3b3bbfbb0049b64a7dff5a6f8",
+    "url": "https://vinojan1999.github.io/csb-rop0v/index.html"
+  },
+  {
+    "revision": "ddb7fa7e264c68d179bf",
+    "url": "https://vinojan1999.github.io/csb-rop0v/static/css/main.b835f712.chunk.css"
+  },
+  {
+    "revision": "e37005127a0255ea5f74",
+    "url": "https://vinojan1999.github.io/csb-rop0v/static/js/2.2d3ac6b1.chunk.js"
+  },
+  {
+    "revision": "ddb7fa7e264c68d179bf",
+    "url": "https://vinojan1999.github.io/csb-rop0v/static/js/main.0437e5fe.chunk.js"
+  },
+  {
+    "revision": "deb66c900743c5f57ffa",
+    "url": "https://vinojan1999.github.io/csb-rop0v/static/js/runtime~main.89db33d8.js"
+  }
+]);
